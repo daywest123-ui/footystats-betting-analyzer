349a651d67ee20373f19fbd961779287e343241b
daywest123-ui <day_west_123@hotmail.com>
2026-09-24 22:25:30 +0300

    Fix calibration bucket regression test

--- app/test_calibration_backtest.py
+++ app/test_calibration_backtest.py
@@ -15,7 +15,7 @@ class CalibrationBacktestTests(unittest.TestCase):
     def test_calibration_table_observed_rate(self):
         points = [CalibrationPoint(0.8, 1), CalibrationPoint(0.8, 0)]
         table = calibration_table(points)
-        self.assertAlmostEqual(table[7]["observed_rate"], 0.5)
+        self.assertAlmostEqual(table[8]["observed_rate"], 0.5)
 
     def test_value_backtest(self):
         points = [
